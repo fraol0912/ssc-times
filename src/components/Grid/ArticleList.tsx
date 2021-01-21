54893de005fd4a4fb672626ebab4e3452bf183c7
fraol0912 <fraol0912@gmail.com>
2021-01-21 23:29:04 +0300

    fix: cards overlapping header

--- src/components/Grid/ArticleList.tsx
+++ src/components/Grid/ArticleList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core/";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { Box } from "@material-ui/core";
 
 // Components
 import ArticleCard from "../Card";
@@ -25,21 +26,23 @@ export default function ArticleList({ articles, tag }: Props) {
   const classes = useStyles();
 
   return (
-    <div
-      role="tabpanel"
-      id={`scrollable-auto-tabpanel-${tag}`}
-      aria-labelledby={`scrollable-auto-tab-${tag}`}
-      className={classes.root}
-    >
-      <Grid container spacing={5}>
-        {articles
-          .filter(article => article.tag === tag)
-          .map(article => (
-            <Grid item xs={12} sm={6} md={4} key={article.id}>
-              <ArticleCard article={article} />
-            </Grid>
-          ))}
-      </Grid>
-    </div>
+    <Box marginTop={5}>
+      <div
+        role="tabpanel"
+        id={`scrollable-auto-tabpanel-${tag}`}
+        aria-labelledby={`scrollable-auto-tab-${tag}`}
+        className={classes.root}
+      >
+        <Grid container spacing={5}>
+          {articles
+            .filter(article => article.tag === tag)
+            .map(article => (
+              <Grid item xs={12} sm={6} md={4} key={article.id}>
+                <ArticleCard article={article} />
+              </Grid>
+            ))}
+        </Grid>
+      </div>
+    </Box>
   );
 }
